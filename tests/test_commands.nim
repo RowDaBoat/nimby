89b05ced7bd829b005bbdf361255327a94bd69b6
Row <reardenr@gmail.com>
2026-08-31 15:13:46 -0300

    fix: windows exe path.

--- tests/test_commands.nim
+++ tests/test_commands.nim
@@ -53,7 +53,7 @@ suite "`nimby use` should":
 
   test "install nim into NIMBY_HOME":
     cmd("nimby use 2.2.10")
-    check fileExists(nimbyHome / "nim" / "bin" / ("nim" & ExeExt))
+    check fileExists(nimbyHome / "nim" / "bin" / addFileExt("nim", ExeExt))
 
   test "fail when nim is not installed":
     removeDir(nimbyHome / "nim")
